f6bde2f949dc3c45df5d1fdad2b70d6f0a6fa9ed
Dinesh babu <dineshbabu142004@gmail.com>
2026-02-06 21:38:17 +0530

    add delete user functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { test, updateUser } from "../controllers/user.controller.js";
+import { deleteUser, test, updateUser } from "../controllers/user.controller.js";
 import upload from "../utils/multer.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -7,6 +7,7 @@ const router = express.Router();
 
 router.get("/test", test);
 router.post("/update/:id",verifyToken,updateUser)
+router.delete("/delete/:id", verifyToken, deleteUser);
 
 // ✅ FIXED Cloudinary upload route (with proper Multer error handling)
 router.post("/upload-avatar", (req, res) => {
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import { useSelector } from 'react-redux';
 import { useRef, useState, useEffect } from 'react';
-import { updateUserStart ,updateUserSuccess,updateUserFailure} from '../redux/user/userSlice';
+import { updateUserStart ,updateUserSuccess,updateUserFailure, deleteUserStart, deleteUserFailure, deleteUserSuccess} from '../redux/user/userSlice';
 import {useDispatch} from 'react-redux';
 export default function Profile() {
   const { currentUser ,loading, error} = useSelector((state) => state.user);
@@ -73,6 +73,23 @@ export default function Profile() {
     catch(error){
       dispatch(updateUserFailure(error.message));
     }
+  };
+  const handleDeleteUser= async()=>{
+    try{
+      dispatch(deleteUserStart());
+      const res= await fetch(`/api/user/delete/${currentUser._id}`,{
+        method:'DELETE',
+      });
+      const data=await res.json();
+      if(data.success == false){
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    }
+    catch(error){
+      dispatch(updateUserFailure(error.message ))
+    }
   }
 
   return (
@@ -137,7 +154,7 @@ export default function Profile() {
       </form>
 
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer">Delete account</span>
+        <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete account</span>
         <span className="text-red-700 cursor-pointer">Sign out</span>
       </div>
       <p className='text-red-700 mt-5'>{error? error:''}</p>
