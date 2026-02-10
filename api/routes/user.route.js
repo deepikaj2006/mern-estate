b82a2980701b3d49b00089a7e9114edcd13f87e4
Dinesh babu <dineshbabu142004@gmail.com>
2026-02-10 21:43:01 +0530

    create get user listings api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,6 +3,7 @@ import {
   deleteUser,
   test,
   updateUser,
+  getUserListings,
 } from "../controllers/user.controller.js";
 import { uploadAvatar } from "../utils/multer.js";
 import { verifyToken } from "../utils/verifyUser.js";
@@ -12,6 +13,7 @@ const router = express.Router();
 router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listings/:id",verifyToken,getUserListings)
 
 /* ===============================
    Upload avatar (Cloudinary)
