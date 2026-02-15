a217ee4a275926679be5883063bcf2daba676551
Dinesh babu <dineshbabu142004@gmail.com>
2026-02-15 21:17:00 +0530

    complete  header  search form functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,30 @@
 import { FaSearch } from "react-icons/fa";
-import {Link} from "react-router-dom";
+import {Link ,useNavigate} from "react-router-dom";
 import {useSelector} from 'react-redux';
+import { useState ,useEffect } from "react";
+
 
 
 export default function Header() {
   const {currentUser} =useSelector(state=>state.user)
+  const [searchTerm,setSearchTerm] = useState('');
+  const navigate= useNavigate();
+  const handleSubmit =(e) =>{
+    e.preventDefault();
+    const urlParams =new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm',searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(()=>{
+     const urlParams = new URLSearchParams(location.search);
+     const searchTermFromUrl=urlParams.get('searchTerm');
+     if(searchTermFromUrl){
+      setSearchTerm(searchTermFromUrl);
+     }
+  },[location.search]);
+
   return (
     //<div>Header</div>
     <header className="bg-slate-200 shadow-md">
@@ -15,13 +35,17 @@ export default function Header() {
             <span className="text-slate-700">Estate</span>
           </h1>
         </Link>
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form onSubmit={handleSubmit} className="bg-slate-100 p-3 rounded-lg flex items-center">
           <input
             type="text"
             placeholder="search...."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <FaSearch className="text-slate-600" />
+          <button>
+            <FaSearch className="text-slate-600" />
+          </button>
         </form>
         <ul className="flex gap-4">
           <Link to="/">
@@ -35,12 +59,17 @@ export default function Header() {
             </li>
           </Link>
           <Link to="/profile">
-            {currentUser?(
-              <img className='rounded-full h-7 w-7 object-cover'src={currentUser.avatar} alt='profile'/>
-            ):(
-            <li className="hidden text-slate-700 sm:inline hover:underline">
-              Sign in
-            </li>)}
+            {currentUser ? (
+              <img
+                className="rounded-full h-7 w-7 object-cover"
+                src={currentUser.avatar}
+                alt="profile"
+              />
+            ) : (
+              <li className="hidden text-slate-700 sm:inline hover:underline">
+                Sign in
+              </li>
+            )}
           </Link>
         </ul>
       </div>
