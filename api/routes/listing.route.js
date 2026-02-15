be46e896cca013c2e3f1589970ee7ad10d702e0d
Dinesh babu <dineshbabu142004@gmail.com>
2026-02-15 19:08:58 +0530

    create Search api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -5,6 +5,7 @@ import {
   deleteListing,
   updateListing,
   getListing,
+  getListings,
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 import { uploadListingImages } from "../utils/multer.js";
@@ -38,6 +39,7 @@ router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id",verifyToken,deleteListing);
 router.post("/update/:id",verifyToken,updateListing);
 router.get('/get/:id',getListing);
+router.get('/get',getListings);
 
 
 export default router;
