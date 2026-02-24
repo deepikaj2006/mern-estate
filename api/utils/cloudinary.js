6e0b3d1ce96074611457110d751d2fc376ca916d
Dinesh babu <dineshbabu142004@gmail.com>
2026-02-24 21:51:21 +0530

    Removed sensitive console logs

--- api/utils/cloudinary.js
+++ api/utils/cloudinary.js
@@ -11,10 +11,6 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-console.log("CLOUDINARY ENV:", {
-  name: process.env.CLOUDINARY_CLOUD_NAME,
-  key: process.env.CLOUDINARY_API_KEY,
-  secret: process.env.CLOUDINARY_API_SECRET,
-});
+
 
 export default cloudinary;
